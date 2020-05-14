4bdeb29bed2634c946f1da1b9fb6bacaa5497071
sukhd <centaurgolden@gmail.com>
2020-05-14 16:37:46 -0500

    added redux store

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import AppNavBar from './components/AppNavBar'
 import List from "./components/List"
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavBar />
-      <List />
-    </div>
+    <Provider store={store}>
+
+
+      <div className="App">
+        <AppNavBar />
+        <List />
+      </div>
+    </Provider>
   );
 }
 
