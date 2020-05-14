63459d8a6dd362b5f23236f43a1d22147c584c1d
sukhd <centaurgolden@gmail.com>
2020-05-14 09:42:46 -0500

    set up simple api

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -5,13 +5,36 @@ const router = express.Router();
 const Item = require('../../models/Item')
 
 //@route GET api/items
-//@description Get all items
+//@description Get all posts
 //@access Public
 
 router.get('/', (req, res) => {
     Item.find()
         .sort({ date: -1 })
         .then(items => res.json(items))
+})
+
+//@route POST api/items
+//@description Create a post
+//@access Public
+
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+    newItem.save()
+        .then(item => res.json(item))
+})
+
+//@route DELETE api/items
+//@description Delete a post
+//@access Public
+
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }))
+
 })
 
 module.exports = router;
