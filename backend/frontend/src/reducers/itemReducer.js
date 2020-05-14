772b2a7f5b0f250264a07b2896e66d657e7b3ef2
sukhd <centaurgolden@gmail.com>
2020-05-14 16:46:40 -0500

    add reducer

---
+++ backend/frontend/src/reducers/itemReducer.js
@@ -0,0 +1,35 @@
+//where state is going to go
+import { v1 as uuid } from "uuid";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+
+const initialState = {
+    items: [
+        {
+            id: uuid(),
+            name: "molk"
+        },
+        {
+            id: uuid(),
+            name: "cereal"
+        },
+        {
+            id: uuid(),
+            name: "pop tarts"
+        },
+        {
+            id: uuid(),
+            name: "waffles"
+        }
+    ]
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case GET_ITEMS:
+            return {
+                ...state
+            }
+        default:
+            return state
+    }
+}
