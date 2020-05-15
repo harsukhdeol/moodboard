f54261621d5a0cb70d12824adc5fd20ef920c174
sukhd <centaurgolden@gmail.com>
2020-05-14 21:44:49 -0500

    connect to mongo?

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = require("./config/keys").mongoURI;
 
 //Connect to Mongo
 mongoose
-  .connect(db, {
+  .connect(db || process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
