d632ade5d533c899ad8c98db4797ff33fddcd140
sukhd <centaurgolden@gmail.com>
2020-05-16 08:47:36 -0500

    displays name

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -35,6 +35,11 @@ class AppNavbar extends Component {
     const authLinks = (
       <Fragment>
         <NavItem>
+          <span className="navbar-text mr-3">
+            <strong>{user ? `Welcome ${user.name}` : null}</strong>
+          </span>
+        </NavItem>
+        <NavItem>
           <Logout />
         </NavItem>
       </Fragment>
