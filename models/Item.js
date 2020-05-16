d3958d726705f609a83b208d1ee4d8b59c11e5f1
sukhd <centaurgolden@gmail.com>
2020-05-16 09:31:24 -0500

    trying to deploy

--- models/Item.js
+++ models/Item.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //create Schema
 const ItemSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  /*  userID:{
+type:String,
+    } */
+});
 
-module.exports = Item = mongoose.model('item', ItemSchema);
+module.exports = Item = mongoose.model("item", ItemSchema);
