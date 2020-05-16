899a93eed1d86afddf4e876bc7eb95be973a468c
sukhd <centaurgolden@gmail.com>
2020-05-15 20:46:35 -0500

    Created refister modal

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -1,49 +1,44 @@
 import React, { Component } from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavLink,
-    Container,
-    NavItem
-} from 'reactstrap';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavLink,
+  Container,
+  NavItem,
+} from "reactstrap";
+
+import RegisterModal from "./auth/registerModal";
 
 class AppNavbar extends Component {
-    state = {
-        isOpen: false
-    }
-    toggle = () => {// using arrow notation automatically binds fxn to this class
-        this.setState(
-            { isOpen: !this.state.isOpen }
-        )
-    }
-    render() {
-        return (
-
-
-            <div>
-                <Navbar color="dark" dark expand="sm" className="mb-5">
-                    <Container>
-                        <NavbarBrand href="/">Harsukh    </NavbarBrand>
-                        <NavbarToggler onClick={this.toggle}></NavbarToggler>
-                        <Collapse isOpen={this.state.isOpen} navbar>
-                            <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <NavLink href="https://github.com/harsukhdeol">
-                                        GitHub
-                </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </Container>
-                </Navbar>
-
-            </div>
-        )
-    }
+  state = {
+    isOpen: false,
+  };
+  toggle = () => {
+    // using arrow notation automatically binds fxn to this class
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">List</NavbarBrand>
+            <NavbarToggler onClick={this.toggle}></NavbarToggler>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <RegisterModal />
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
 
 export default AppNavbar;
