0b4562289d4827a403d59ab6da4bf74c58969fc0
sukhd <centaurgolden@gmail.com>
2020-05-17 10:19:20 -0500

    removed build from .gitignore

---
+++ client/build/precache-manifest.b5be2882217bd730510519b112a5dd88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397d2e4dfbe5b6b8be3c1e2ccafc7cbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ff6f96c836f1a9b6dec",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c1fc40eb9e2b25dfbd17",
+    "url": "/static/css/main.5f623338.chunk.css"
+  },
+  {
+    "revision": "6ff6f96c836f1a9b6dec",
+    "url": "/static/js/2.dffc2a3a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dffc2a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1fc40eb9e2b25dfbd17",
+    "url": "/static/js/main.688ddeab.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
